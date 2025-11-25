90c931608c60e723a4c8557c4face8b146b22227
Hector <hectorbiminchumo@gmail.com>
2025-11-25 15:40:58 -0500

    solving Plus one and add readme

---
+++ arrays/easy/066-PlusOne/PlusOne.js
@@ -0,0 +1,18 @@
+
+const PlusOne = (digits) => {
+
+   for(let i=digits.length-1;i>=0;i--){
+        digits[i]++
+
+        if(digits[i] < 10) {
+            return digits
+        }
+        digits[i] = 0
+   }
+
+   digits.unshift(1);
+   return digits
+
+
+
+}
