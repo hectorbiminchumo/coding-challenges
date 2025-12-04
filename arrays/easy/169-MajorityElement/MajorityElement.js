d1490d70aa9e70ce9de590c1785691599863434b
Hector <hectorbiminchumo@gmail.com>
2025-12-03 21:26:42 -0500

    adding MajorityElement

---
+++ arrays/easy/169-MajorityElement/MajorityElement.js
@@ -0,0 +1,16 @@
+const MajorityElement = (nums) => {
+    let candidate=null
+    let count = 0
+
+    for(let num of nums){
+        if(count === 0){
+            candidate = num
+        }
+        if(num === candidate){
+            count++
+        }else{
+            count--
+        }
+    }
+    return candidate
+}
